528df179571e068cf3d783d1c5db9a93b94fa905
rustamovilyos <rustamov7871@gmail.com>
2023-10-11 00:11:08 +0500

    added new actions

--- actions/actions.py
+++ actions/actions.py
@@ -4,17 +4,19 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 import random
-# This is a simple example for a custom action which utters "Hello World!"
-
+import re
 from typing import Any, Text, Dict, List
 
 import yaml
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import (
+    UserUtteranceReverted,
+    ConversationPaused,
+)
 from rasa_sdk.executor import CollectingDispatcher
 
 
 class ActionCheckQuestions(Action):
-
     def name(self) -> Text:
         return "action_check_exist_questions"
 
@@ -22,17 +24,55 @@ class ActionCheckQuestions(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         user_message = tracker.latest_message['text']
+        questions_1 = set()
+        with open('../data/nlu.yml', 'r') as questions_file:
+            questions = yaml.safe_load(questions_file)
+            for intent_data in questions.get("nlu", []):
+                intent_examples = intent_data.get("examples", "")
+                intent_examples = re.split(r'[\n]', intent_examples.replace("- ", "").lower())
+                questions_1.update(intent_examples)
 
-        with open('../data/nlu.yml', 'r') as questions:
-            questions = yaml.safe_load(questions)
-        possible_answers = ['Iltimos savolingizni boshqacharoq shaklda bersangiz!',
-                            "Men bu savolingiz haqida o'ylab olgunimcha boshqa savol berishingizni so'rayman :)",
-                            "Savolingizni boshqacharoq shaklda bersangiz, iltimos!",
-                            "Men tushunmaydigan savollar bermasangiz yaxshi bo'ladi :)", ]
-        for intent_num in range(len(questions["nlu"])):
-            intent_examples = questions["nlu"][intent_num]["examples"]
-            if user_message in intent_examples:
-                continue
-            elif user_message not in intent_examples and tracker.latest_message['intent']['confidence'] < 0.65:
-                    dispatcher.utter_message(text=possible_answers[random.randint(0, len(possible_answers) - 1)])
+        if user_message.lower() in questions_1:
+            dispatcher.utter_message(text="well done!")
+        else:
+            dispatcher.utter_message(text="Iltimos savolingizni boshqacharoq shaklda bersangiz!")
+
         return []
+
+
+
+#
+#
+
+# import openai
+#
+# openai.api_key = ""
+
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher, tracker, domain):
+        # output a message saying that the conversation will now be
+        # continued by a human.
+        current_state = tracker.current_state()
+        latest_message = current_state["latest_message"]["text"]
+        # context = "As an operator of 'Beeline' company, you are talking to a 'beeline' client and you must give answers in Uzbek"
+        # message = latest_message
+        # response = openai.ChatCompletion.create(
+        #     model="gpt-3.5-turbo",
+        #     messages=[
+        #         {"role": "system", "content": context},
+        #         {"role": "user", "content": message},
+        #     ],
+        # )
+
+        # reply = response["choices"][0]["message"]["content"]
+        # print(reply)
+        # reply = response["message"]["content"]
+        message = "Kechirasiz! sizni nima demoqchi ekanligingizni tushuna olmadim."
+        dispatcher.utter_message(text=message)
+        # pause tracker
+        # undo last user interaction
+        return [ConversationPaused(), UserUtteranceReverted()]
